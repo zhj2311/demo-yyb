06d671b428c6e934cbdba5a294c31c6ad48224d9
haojie2311 <2969269100@qq.com>
2022-03-16 21:38:52 +0800

    前端文件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+//饿了么ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// 关闭浏览器对环境的提示
 Vue.config.productionTip = false
 
 new Vue({
